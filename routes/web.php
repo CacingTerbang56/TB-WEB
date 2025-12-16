3208e2ac76fd19d97d328a16f990f2b920548016
CacingTerbang56 <hamdanarsala123@gmail.com>
2025-12-16 10:46:20 +0700

    tambah form login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/admin/dashboard', function () {
     return view('/admin/dashboard');
 });
